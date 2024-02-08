3564627a8710eea2d4ccea36a374c51c0af84b44
Colin ML Burnett <cmlburnett@gmail.com>
2024-02-08 14:40:27 -0600

    Untested, added ZipMan to deal with processing WIFF to a ZIP file

--- wiffecg/__init__.py
+++ wiffecg/__init__.py
@@ -1,4 +1,8 @@
 import datetime
+import enum
+import os
+import pickle
+import zipfile
 
 import wiff
 import matplotlib
@@ -6,6 +10,7 @@ from matplotlib import pyplot
 from matplotlib.backends.backend_pdf import PdfPages
 
 import pyiworxekgedfimport
+from pyzestyecg import pyzestyecg
 
 class WIFFECG:
 	def __init__(self, fname):
@@ -162,4 +167,309 @@ class WIFFECG:
 				times.append(f[0]/freq)
 				for i in range(len(leads)):
 					vals[i].append(f[2][i])
+
+	def ProcessRRToZip(self, fname, params, intervals, savepng=True, savepdf=True):
+		"""
+		Process the RR intervals and use a zip file to store all the results.
+		@intervals should be a dictionary
+			'user'		user defined intervals, a dictionary mapping to a list of (start,stop) tuples
+			'noise'		list of (start,stop) tuples that indicate noise
+			'ignore'	list of (start,stop) tuples that are ignored entirely
+		@savepng indicates if PNG images of the EKG should be saved
+		@savepdf indicates if PDF file of the EKG with peaks should be saved
+		"""
+
+		# Get sampling rate for the recording
+		samp = self.wiff.recording[1].sampling
+
+		# intervals are in seconds, convert to framex by multiplying by the sampling rate
+		# Round to the nearest frame
+		intervals_user_frames = {}
+		for k,v in intervals['user'].items():
+			intervals_user_frames[k] = [(round(_[0]*samp),round(_[1]*samp)) for _ in intervals]
+
+		intervals_ignore_frames = []
+		for v in intervals['ignore']:
+			intervals_ignore_frames.append( (round(_[0]*samp),round(_[1]*samp)) )
+
+		intervals_noise_frames = []
+		for v in intervals['noise']:
+			intervals_noise_frames.append( (round(_[0]*samp),round(_[1]*samp)) )
+
+		print(intervals_user_frames)
+		print(intervals_ignore_frames)
+		print(intervals_noise_frames)
+		raise NotImplementedError
+
+		with ZipMan(fname) as z:
+			if z.IsStateEmpty:
+				# Start processing from the start, do any initializing
+				z.State['Channels'] = self.GetLeads()
+				z.State['Potentials'] = None
+				z.State['Peaks'] = None
+				z.State['Correlate'] = None
+				z.State['Keep'] = None
+				z.State['Remove'] = None
+				z.State['UserFilter'] = None
+				z.SetStatInitialized()
+				z.SaveState()
+
+			elif z.IsStateInitialized:
+				p = pyzestyecg(self.wiff, params)
+				potentials, peaks = p.GetPotentialsAndPeaks(intervals_ignore_frames, intervals_noise_frames)
+
+				z.State['Potentials'] = potentials
+				z.State['Peaks'] = peaks
+				z.SetStateCorrelate()
+				z.SaveState()
+
+			elif z.IsStateCorrelate:
+				chans = z.State['Channels']
+				potentials = z.State['Potentials']
+				peaks = z.State['Peaks']
+
+				p = pyzestyecg(self.wiff)
+				correlate = p.GetCorrelate(chan, potentials, peaks)
+
+				z.State['Correlate'] = correlate
+				z.SetStateKeepKeys()
+				z.SaveState()
+
+			elif z.IsStateKeepKeys:
+				chans = z.State['Channels']
+				peaks = z.State['Peaks']
+
+				p = pyzestyecg(self.wiff)
+				points, keep = p.GetKeepKeys(chans, peaks)
+
+				z.State['Keep'] = keep
+				z.State['Points'] = points
+				z.SetStateRemoveKeys()
+				z.SaveState()
+
+			elif z.IsStateRemoveKeys:
+				chans = z.State['Channels']
+				correlate = z.State['Correlate']
+				points = z.State['Points']
+				keep = z.State['Keep']
+
+				p = pyzestyecg(self.wiff)
+				remove = p.GetRemoveKeys(chans, correlate, points, keep)
+
+				z.State['Remove'] = remove
+				z.SetStateUserFilter()
+				z.SaveState()
+
+			elif z.IsStateUserFilter:
+				chans = z.State['Channels']
+				points = z.State['Points']
+				keep = z.State['Keep']
+				remove = z.State['Remove']
+				user = z.State['UserFilter']
+
+				p = pyzestyecg(self.wiff)
+				p.CheckUserFilter(chans, points, keep, remove, user)
+
+				z.SetStateCalculateRR()
+				z.SaveState()
+
+			elif z.IsStateCalculateRR:
+				chans = z.State['Channels']
+				correlate = z.State['Correlate']
+				keep = z.State['Keep']
+				remove = z.State['Remove']
+				user = z.State['UserFilter']
+
+				p = pyzestyecg(self.wiff)
+				p.CalculateRR(chans, keep, remove, user, intervals_user_frames, intervals_noise_frames, params)
+
+				if savepng:
+					z.SetStateSavePNG()
+				elif savepdf:
+					z.SetStateSavePDF()
+				else:
+					z.SetStateCompleted()
+				z.SaveState()
+
+
+			elif z.IsStateSavePNG:
+				print("Save as PNG")
+
+				if savepdf:
+					z.SetStateSavePDF()
+				else:
+					z.SetStateCompleted()
+				z.SaveState()
+
+			elif z.IsStateSavePDF
+				print("Save as PDF")
+
+				z.SetStateCompleted()
+				z.SaveState()
+
+			elif z.IsStateCompleted:
+				raise NotImplementedError("Should not reach this point")
+
+			else:
+				raise NotImplementedError("Should not reach this point")
+
+class ZipMan:
+	class ProcessingStateEnum(enum.EnumInt):
+		EMPTY = 0
+		INITIALIZED = 1
+		CORRELATE = 2
+		KEEPKEYS = 3
+		REMOVEKEYS = 4
+		USERFILTER = 5
+		CALCULATERR = 6
+		SAVEPNG = 7
+		SAVEPDF = 8
+		COMPLETED = 100
+
+		ERROR = 1000
+
+	def __init__(self, fname):
+		self._fname = fname
+		self._zip = None
+		self._state = {
+			'state': ZipMan.ProcessingStateEnum.EMPTY,
+			'error': None,
+		}
+
+	@property
+	def Filename(self):
+		return self._fname
+
+	@property
+	def Zip(self):
+		return self._zip
+
+	@property
+	def State(self):
+		return self._state
+
+
+	@property
+	def IsStateEmpty(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.EMPTY
+
+	@property
+	def IsStateInitialized(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.INITIALIZED
+
+	@property
+	def IsStateCorrelate(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.CORRELATE
+
+	@property
+	def IsStateKeepKeys(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.KEEPKEYS
+
+	@property
+	def IsStateRemoveKeys(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.REMOVEKEYS
+
+	@property
+	def IsStateUserFilter(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.USERFILTER
+
+	@property
+	def IsStateCalculateRR(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.CALCULATERR
+
+	@property
+	def IsStateSavePNG(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.SAVEPNG
+
+	@property
+	def IsStateSavePDF(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.SAVEPDF
+
+	@property
+	def IsStateCompleted(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.COMPLETED
+
+	@property
+	def IsStateNonError(self):
+		return self._state['state'] != ZipMan.ProcessingStateEnum.ERROR
+
+	@property
+	def IsStateError(self):
+		return self._state['state'] == ZipMan.ProcessingStateEnum.ERROR
+
+
+	def SetStatEmpty(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.EMPTY
+
+	def SetStatInitialized(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.INITIALIZED
+
+	def SetStateCorrelate(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.CORRELATE
+
+	def SetStateKeepKeys(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.KEEPKEYS
+
+	def SetStateRemoveKeys(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.REMOVEKEYS
+
+	def SetStateUserFilter(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.USERFILTER
+
+	def SetStateCalculateRR(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.CALCULATERR
+
+	def SetStateSavePNG(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.SAVEPNG
+
+	def SetStateSavePDF(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.SAVEPDF
+
+	def SetStateCompleted(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.COMPLETED
+
+	def SetStateError(self):
+		self._state['state'] = ZipMan.ProcessRRToZip.ERROR
+
+	def SetError(self, e, tb, msg, dat=None):
+		"""
+		Set the state to error and record the exception instance @e, the traceback @tb, and user message @msg.
+		Optional is a data object @dat to include (eg, a dictionary of variables)
+		"""
+		self.SetStateError()
+		self._state['error'] = {'exception': e, 'traceback': tb, 'message': msg, 'data': dat}
+
+	def open(self):
+		if self._zip:
+			raise Exception("File is already open")
+
+		self._zip = zipfile.ZipFile(self.Filename, 'a')
+
+		# Write empty state if not existant or read what's there
+		if 'state.pypickle' not in z.namelist():
+			with self.Zip.open('state.pypickle', 'w') as f:
+				pickle.dump(self.State, f)
+		else:
+			with self.Zip.open('state.pypickle', 'r') as f:
+				self._state = pickle.load(f)
+
+	def close(self):
+		if self._zip is None:
+			raise Exception("File is not open, cannot close it")
+
+		self._zip.close()
+		self._zip = None
+
+	def SaveState(self):
+		# Save the state
+		with self.Zip.open('state.pypickle', 'w') as f:
+			pickle.dump(self.State, f)
+
+	def __enter__(self):
+		self.open()
+
+	def __exit__(self, *kargs):
+		if self.Zip:
+			self.close()
+		return False
 
